1e12b1721c47b78fe753ebd9421699031e8ae22b
sueastward <sueastward@outlook.com>
2020-07-18 17:18:35 +0800

    Core/Battlefield: Fixed crash in wintergrasp setup revealed after 4c173e4b7b35161fcaaa4917da8fde2e4f3cbdd8 (#25072)

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -414,7 +414,7 @@ bool BattlefieldWG::SetupBattlefield()
     m_BattleId = BATTLEFIELD_BATTLEID_WG;
     m_ZoneId = AREA_WINTERGRASP;
     m_MapId = BATTLEFIELD_WG_MAPID;
-    m_Map = sMapMgr->FindMap(m_MapId, 0);
+    m_Map = sMapMgr->CreateBaseMap(m_MapId);
 
     InitStalker(BATTLEFIELD_WG_NPC_STALKER, WintergraspStalkerPos);
 
