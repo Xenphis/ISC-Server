acffc0c13cfed0fe76ee64f6b83eac7a61c94ffc
Shauren <shauren.trinity@gmail.com>
2024-03-19 13:24:23 +0100

    Core/Logging: Fix intellisense errors - people don't like it when code glows red without a reason

--- src/server/game/AuctionHouseBot/AuctionHouseBotSeller.cpp
+++ src/server/game/AuctionHouseBot/AuctionHouseBotSeller.cpp
@@ -357,12 +357,15 @@ bool AuctionBotSeller::Initialize()
 
     LoadConfig();
 
-    TC_LOG_DEBUG("ahbot", "Items loaded \tGray\tWhite\tGreen\tBlue\tPurple\tOrange\tYellow");
-    for (uint32 i = 0; i < MAX_ITEM_CLASS; ++i)
-        TC_LOG_DEBUG("ahbot", "\t\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
-        (uint32)_itemPool[0][i].size(), (uint32)_itemPool[1][i].size(), (uint32)_itemPool[2][i].size(),
-        (uint32)_itemPool[3][i].size(), (uint32)_itemPool[4][i].size(), (uint32)_itemPool[5][i].size(),
-        (uint32)_itemPool[6][i].size());
+    if (sLog->ShouldLog("ahbot", LOG_LEVEL_DEBUG))
+    {
+        sLog->OutMessage("ahbot", LOG_LEVEL_DEBUG, "Items loaded \tGray\tWhite\tGreen\tBlue\tPurple\tOrange\tYellow");
+        for (uint32 i = 0; i < MAX_ITEM_CLASS; ++i)
+            sLog->OutMessage("ahbot", LOG_LEVEL_DEBUG, "\t\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+            _itemPool[0][i].size(), _itemPool[1][i].size(), _itemPool[2][i].size(),
+                _itemPool[3][i].size(), _itemPool[4][i].size(), _itemPool[5][i].size(),
+                _itemPool[6][i].size());
+    }
 
     TC_LOG_DEBUG("ahbot", "AHBot seller configuration data loaded and initialized");
     return true;
